67f4316019709c0205a1ce78bb6211635d4890be
KIMURAHIROTO <shiro7011@gmail.com>
2021-07-06 12:39:16 +0000

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,24 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 被フォロー関係を通じて参照→followed_idをフォローしている人
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 【class_name: "Relationship"】は省略可能0  has_many :followings, through: :relationships, source: :followed
+  has_many :followings, through: :relationships, source: :followed
+  # 与フォロー関係を通じて参照→follower_idをフォローしている人
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    followings.include?(user)
+  end
+
   validates :name, uniqueness: true, presence: true,length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 end
